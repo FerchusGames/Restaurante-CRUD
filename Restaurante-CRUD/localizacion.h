06a4ff032c3436d773ddec608280c3bd5d2230ed
richdev2021 <94477023+richdev2021@users.noreply.github.com>
2022-04-01 10:58:04 -0600

    localiszion v1.0

---
+++ Restaurante-CRUD/localizacion.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class loclizacion
+{
+public:	
+	string planeta = "tierra", pais="mexico", estado="jalisco", ciudad="guadalajara", municipio="zapopan", colonia="las aguilas", calle="rio tuxpan";
+	int numero = 85;
+private:
+	int coordenadas;
+};
+
+loclizacion::loclizacion()
+{
+
+}
+

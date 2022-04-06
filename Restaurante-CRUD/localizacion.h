e34f6cacc7b6caf192effaa7c2719e078446e961
richdev2021 <94477023+richdev2021@users.noreply.github.com>
2022-04-06 08:30:56 -0500

    agregue valor a las coordenadas
    un valor base para las coordenadas

--- Restaurante-CRUD/localizacion.h
+++ Restaurante-CRUD/localizacion.h
@@ -8,10 +8,11 @@ private:
 	string planeta = "tierra", pais="mexico", estado="jalisco", ciudad="guadalajara", municipio="zapopan", colonia="las aguilas", calle="rio tuxpan";
 public:	
 	int numero = 85;
-	int coordenadas;
-	loclizacion(string,string,string,string,string,string,string, int, int);
+	int coordenadas = 20;
+	int coordenados = -103;
+	loclizacion(string,string,string,string,string,string,string, int, int,int);
 };
-loclizacion::loclizacion(string _planeta ,string _pais ,string _estado,string _ciudad, string _municipio, string _colonia, string _calle, int _numero, int _coordenadas) 
+loclizacion::loclizacion(string _planeta ,string _pais ,string _estado,string _ciudad, string _municipio, string _colonia, string _calle, int _numero, int _coordenadas,int _coordenados) 
 {
 	planeta = _planeta;
 	pais = _pais;
@@ -22,7 +23,7 @@ loclizacion::loclizacion(string _planeta ,string _pais ,string _estado,string _c
 	calle = _calle;
 	numero = _numero;
 	coordenadas = _coordenadas;
-
+	coordenados = _coordenados;
 }
 int main()
 {

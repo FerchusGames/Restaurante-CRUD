098ebe4af8e8b37200cb555081d4465a0bcfe9ee
Ferchus <ferchusgames@protonmail.com>
2022-04-06 08:42:46 -0500

    Proovedor V2

--- Restaurante-CRUD/Provedor.h
+++ Restaurante-CRUD/Provedor.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include<iostream>
 #include<string>
 
-class Provedor
+class Proveedor
 {
 public:
-	Provedor(std::string _nombre, std::string _recurso, float _costoAnual, std::string _localizacion, std::string _metodoTransporte);
+	Proveedor(std::string _nombre, std::string _recurso, float _costoAnual, std::string _localizacion, std::string _metodoTransporte);
 
 private:
 	std::string nombre;
@@ -15,12 +16,35 @@ private:
 	std::string metodoTransporte;
 };
 
-Provedor::Provedor(std::string _nombre, std::string _recurso, float _costoAnual, std::string _localizacion, std::string _metodoTransporte)
+Proveedor::Proveedor(std::string _nombre, std::string _recurso, float _costoAnual, std::string _localizacion, std::string _metodoTransporte)
 {
 	nombre = _nombre;
 	recurso = _recurso;
 	costoAnual = _costoAnual;
 	localizacion = _localizacion;
 	metodoTransporte = _metodoTransporte;
+}
+
+void CrearProovedor()
+{
+	std::string _nombre;
+	std::string _recurso;
+	float _costoAnual;
+	std::string _localizacion;
+	std::string _metodoTransporte;
+
+	std::cout << "Creador de Proovedor: \n\n";
+	std::cout << "Ingresa el nombre: ";
+	std::getline(std::cin, _nombre);
+	std::cout << "Ingresa el recurso que proovee: ";
+	std::getline(std::cin, _recurso);
+	std::cout << "Ingresa el costo anual del proovedor:	";
+	std::cin >> _costoAnual;
+	std::cout << "Ingresa el lugar del que proovee: ";
+	std::getline(std::cin, _localizacion);
+	std::cout << "Ingresa el metodo de transporte de los recursos";
+	std::getline(std::cin, _metodoTransporte);
+
+	nuevoProovedor = new Provedor(_nombre, _recurso, _costoAnual, _localizacion, _metodoTransporte);
 }
 
